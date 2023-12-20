77c17d411f031d5233d145a2f0e0dbc64166fc01
Floorban <15696082812@163.com>
2023-12-20 10:26:58 +0100

    tiltinput

--- Assets/Scripts/PerformerController.cs
+++ Assets/Scripts/PerformerController.cs
@@ -4,6 +4,8 @@ public class PerformerController : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
+    [SerializeField]
+    private float tiltSpeed = 20f;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -14,8 +16,10 @@ public class PerformerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         float move_amount = Mathf.Clamp01(Mathf.Abs(movement.x) + Mathf.Abs(movement.z));
-        animator.SetFloat("animSpeed", move_amount, 100f, 1f);
         animator.SetFloat("animStyle", move_amount, 100f, 1f);
+
+        float tiltInput = Input.acceleration.y * tiltSpeed;
+        animator.SetFloat("animSpeed", Mathf.Clamp01(tiltInput), 100f, Time.deltaTime);
 
     }
 }
