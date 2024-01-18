f5ee3337e83c9b27763c04bca61b1efb0a002dad
Floorban <15696082812@163.com>
2024-01-18 11:10:35 +0100

    ok

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -11,7 +10,21 @@ public class GameManager : MonoBehaviour
     private float startTime;
     [SerializeField]
     private TextMeshProUGUI text;
-    void Update()
+
+    [SerializeField]
+    private int volumeLevel, pitchLevel, moveLevel;
+    [SerializeField]
+    private TextMeshProUGUI volumeText, pitchText, moveText;
+
+    [SerializeField]
+    private Image countDownBar;
+    [SerializeField]
+    private float countTimer;
+    private void Start()
+    {
+        countTimer = 10f;
+    }
+    private void Update()
     {
         text.text = startTime.ToString("0.00");
 
@@ -25,8 +38,10 @@ public class GameManager : MonoBehaviour
             text.color = Color.green;
             collider1Hit = false;
             collider2Hit = false;
-           
         }
+
+        RandomTask();
+        CountDownBar();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -41,4 +56,30 @@ public class GameManager : MonoBehaviour
             collider2Hit = true;
         }
     }
+
+    private void RandomTask()
+    {
+        if (countTimer == 10f)
+        {
+            volumeLevel = Random.Range(0, 10);
+            pitchLevel = Random.Range(0, 10);
+            moveLevel = Random.Range(0, 10);
+
+            volumeText.text = $"Volume: + {volumeLevel.ToString()}";
+            pitchText.text = $"Pitch: + {pitchLevel.ToString()}";
+            moveText.text = $"Movement: + {moveLevel.ToString()}";
+        }
+    }
+    private void CountDownBar()
+    {
+        countDownBar.fillAmount = countTimer / 10f;
+
+        countTimer -= Time.deltaTime;
+
+        if (countTimer <= 0f)
+        {
+            countTimer = 10f;
+        }
+    }
 }
+
