ae205be84a8ab463777e9edd62ea9b1574d6615a
Floorban <15696082812@163.com>
2024-01-23 10:47:27 +0100

    load screen

--- Assets/LoadbarController.cs
+++ Assets/LoadbarController.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class LoadbarController : MonoBehaviour
 {
-    public GameObject doneTextholder;
     public Image loadbarForeground;
     public float timeRemaining;
-    public float max_time = 5.0f;
-    public GameObject loadbarTextHolder;
+    public float max_time = 10f;
+    public TextMeshProUGUI text;
 
     void Start()
     {
@@ -24,16 +24,31 @@ public class LoadbarController : MonoBehaviour
             timeRemaining += Time.deltaTime;
             loadbarForeground.fillAmount = timeRemaining / max_time;
         }
-        else
-        {
-            doneTextholder.SetActive(true);
-            loadbarTextHolder.SetActive(false);
-        }
-
         if (timeRemaining >= max_time + 1f)
         {
-            Debug.Log("lol");
             SceneManager.LoadScene(2);
+        }
+
+        switch ((int)timeRemaining)
+        {
+            case 0:
+                text.text = ("The audience is entering...");
+                break;
+            case 3:
+                text.text = ("Putting on the performing suit...");
+                break;
+            case 6:
+                text.text = ("Do you know that uConduct was made within one week...");
+                break;
+            /*case 6:
+                text.text = ("Swipe the screen to change the pitch...");
+                break;*/
+            case 7:
+                text.text = ("Looking for the music sheet and conducting wand...");
+                break;
+            case 10:
+                text.text = ("Turning the phone to silent...");
+                break;
         }
 
     }
