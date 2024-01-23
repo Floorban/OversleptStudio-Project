fcaff358385d8d5ed65ac6e0e5d9213c18dd6004
Floorban <15696082812@163.com>
2024-01-23 14:09:51 +0100

    m

--- Assets/Scripts/ShareButton.cs
+++ Assets/Scripts/ShareButton.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class ShareButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ShareScreen()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ScreenCapture.CaptureScreenshot("screenshot.png");
+        //
     }
 }
